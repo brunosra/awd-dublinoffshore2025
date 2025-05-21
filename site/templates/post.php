18ae9a71c526c00cde6ed57e11d00ba6151ae91a
aengustukel <84077392+aengustukel@users.noreply.github.com>
2025-05-21 21:17:30 +0100

    Build out indivisual blog posts Adding plugins

--- site/templates/post.php
+++ site/templates/post.php
@@ -1,57 +1,108 @@
 <?php snippet('header') ?>
 
-<section class="postTitle container">
-  <div class="grid-spaceBetween">
-    <div class="col">
-      <h4>Updates</h4>
-    </div>
-    <div class="col">
-      <a href="<?= $page->parent()->url() ?>" class="inlinelink align-text-bottom">&larr; Back to Updates</a>
-    </div>
-  </div>
+<section class="">
+  <h5><?= $page->parent()->title()->esc() ?></h5>
+  <h1><?= $page->title()->html() ?></h1>
+  <?php if ($page->cover()->isNotEmpty()): ?>
+  <?php foreach ($page->cover()->toFiles() as $image): ?>
+    <?php
+    $sizes = "(min-width: 1200px) 50vw,
+              (min-width: 900px) 33vw,
+              (min-width: 600px) 50vw,
+              100vw";
+    ?>
+    <picture>
+      <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+      <img
+          class=""
+          alt="<?= $image->alt() ?>"
+          data-src="<?= $image->resize(300)->url() ?>"
+          data-srcset="<?= $image->srcset() ?>"
+          sizes="<?= $sizes ?>"
+          width="<?= $image->resize(310)->width() ?>"
+          height="<?//= $image->resize(500)->height() ?>">
+    </picture>
+  <?php endforeach ?>
+<?php else: ?>
+<!-- is empty, no placeholder image -->
+<?php endif ?>
 </section>
 
-<section>
+<aside>
+  <?php foreach ($page->tags() as $tag): ?>
+    <p class="small"><?= $tag ?></p>
+  <?php endforeach ?>
+    <p class="small"><?= $page->published()->toDate('Y') ?></p>
+    <ul class="">
+      <li>
+        <a width="38px" height="38px" rel="nofollow" onClick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?= $page->url() ?>','sharer','toolbar=0,status=0,width=580,height=325');" href="javascript: void(0)">
+          <img src="<?= url('assets/images/facebook.png') ?>" width="22px" height="22px" alt="Share on Facebook"/>
+        </a>
+      </li>
+      <li>
+        <a width="38px" height="38x" href="https://x.com/intent/tweet?<?= $page->url() ?>" target="blank" rel="noopener noreferrer nofollow">
+          <img src="<?= url('assets/images/twitter.png') ?>"alt="Share Page on X"/>
+        </a>
+      </li>
+      <li>
+        <a width="38x" height="38px" href="https://www.linkedin.com/shareArticle?<?= $page->url() ?>" target="blank" rel="noopener noreferrer nofollow">
+          <img src="<?= url('assets/images/linkedin.png') ?>" alt="Share Page on LinkedIn"/></a>
+      </li>
+    </ul>
+</aside>
   
-  <div class="container tight-padding">
-    <div class="post">
-      <div class="sidebar">
-        <h4><?= $page->title() ?></h4>
-        <div class="col-12 data">
-          <?php foreach ($page->tags() as $tag): ?>
-          <p class="tag"><?= $tag ?></p>
-          <?php endforeach ?>
-          <p class="date"><?= $page->published()->toDate('Y') ?></p>
-          <hr />
-          <p>Share</p>
-          <ul class="social-share">
-            <li>
-              <a width="24px" height="24px" rel="nofollow" onClick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?= $page->url() ?>','sharer','toolbar=0,status=0,width=580,height=325');" href="javascript: void(0)">
-                <img src="<?= url('assets/images/facebook.png') ?>" width="22px" height="22px" alt="Share on Facebook"/>
-              </a>
-            </li>
-            <li>
-              <a width="24px" height="20px" href="https://twitter.com/intent/tweet?<?= $page->url() ?>" target="blank" rel="noopener noreferrer nofollow">
-                <img src="<?= url('assets/images/twitter.png') ?>"alt="Share Page on Twitter"/>
-              </a>
-            </li>
-            <li>
-              <a width="24px" height="24px" href="https://www.linkedin.com/shareArticle?<?= $page->url() ?>" target="blank" rel="noopener noreferrer nofollow">
-                <img src="<?= url('assets/images/linkedin.png') ?>" alt="Share Page on LinkedIn"/></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div class="main responsive">
-        <?php foreach ($page->postContent()->toBlocks() as $block): ?>
-        <div class="block">
-          <?= $block ?>
-        </div>
-        <?php endforeach ?>
-      </div>
-    </div>
-  </div>
-
+<section>
+<?php foreach ($page->postContent()->toBlocks() as $block): ?>
+    <?= $block ?>
+<?php endforeach ?>
 </section>
 
 <?php snippet('footer') ?>
+
+
+<section>
+  <h5><?= $page->parent()->title()->esc() ?></h5>
+  <h5><a><?= $page->parent()->url() ?></a></h5>
+
+  <?php
+    $sizes = "(min-width: 1200px) 25vw,
+              (min-width: 900px) 33vw,
+              (min-width: 600px) 50vw,
+              100vw";
+
+    $related = $page->related()->toPages();
+    foreach($related as $post): ?>
+    <article>
+      <a class="" href="<?= $post->url() ?>">
+          <?php if ($post->cover()->isNotEmpty()): ?>
+            <?php foreach ($post->cover()->toFiles() as $image): ?>
+              <?php
+              $sizes = "(min-width: 1200px) 50vw,
+                        (min-width: 900px) 33vw,
+                        (min-width: 600px) 50vw,
+                        100vw";
+              ?>
+              <picture>
+                <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+                <img
+                    class=""
+                    alt="<?= $image->alt() ?>"
+                    data-src="<?= $image->resize(300)->url() ?>"
+                    data-srcset="<?= $image->srcset() ?>"
+                    sizes="<?= $sizes ?>"
+                    width="<?= $image->resize(430)->width() ?>"
+                    height="<?//= $image->resize(500)->height() ?>">
+              </picture>
+            <?php endforeach ?>
+          <?php else: ?>
+          <!-- Hard coded location of placeholder image here  <?//= url('assets/images/#.webp') ?>-->
+          <?php endif ?>
+          <p class="small"><?= $post->published()->toDate('Y') ?></p>
+          <?php foreach ($post->tags() as $tag): ?>
+            <p class="small"><?= $tag ?></p>
+          <?php endforeach ?>
+          <h6 class=""><?= $post->title()->esc() ?></h6>
+      </a>  
+    </article>
+  <?php endforeach ?>
+</section>
