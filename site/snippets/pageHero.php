03c527839b957c23829552fed20253861175ec12
aengustukel <84077392+aengustukel@users.noreply.github.com>
2025-05-24 12:47:20 +0100

    First Draft of page hero php strcuture

--- site/snippets/pageHero.php
+++ site/snippets/pageHero.php
@@ -1,10 +1,11 @@
-<section class="">
-  <?php if ($page->parent('updates')): ?>
+<?php $isUpdate = $page->parent('updates'); ?>
+
+<?php if ($isUpdate): ?>
   <h5><?= $page->parent()->title()->esc() ?></h5>
-  <?php endif ?>
   <h1><?= $page->title()->html() ?></h1>
-  <?php if ($page->parent('updates') && $page->cover()->isNotEmpty()): ?>
-  <?php foreach ($page->cover()->toFiles() as $image): ?>
+
+  <?php if ($page->cover()->isNotEmpty()): ?>
+      <?php foreach ($page->cover()->toFiles() as $image): ?>
     <?php
     $sizes = "(min-width: 1200px) 50vw,
               (min-width: 900px) 33vw,
@@ -22,8 +23,34 @@
           width="<?= $image->resize(310)->width() ?>"
           height="<?//= $image->resize(500)->height() ?>">
     </picture>
-  <?php endforeach ?>
-<?php else: ?>
-<!-- is empty, no placeholder image -->
+    <?php endforeach ?>
+  <?php else: ?>
+    <!-- no placeholder image -->
+  <?php endif ?>
+
+<?php elseif (isset($block)): ?>
+  <?php if ($block->background()->isNotEmpty()): ?>
+    <?php foreach ($block->background()->toFiles() as $image): ?>
+      <?php
+      $sizes = "(min-width: 1200px) 50vw,
+                (min-width: 900px) 33vw,
+                (min-width: 600px) 50vw,
+                100vw";
+      ?>
+      <picture>
+        <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+        <img
+          class=""
+          alt="<?= $image->alt() ?>"
+          data-src="<?= $image->resize(300)->url() ?>"
+          data-srcset="<?= $image->srcset() ?>"
+          sizes="<?= $sizes ?>"
+          width="<?//= $image->resize(310)->width() ?>"
+          height="<?//= $image->resize(500)->height() ?>">
+      </picture>
+    <?php endforeach ?>
+  <?php else: ?>
+    <!-- no background image -->
+  <?php endif ?>
+  <h1><?= $block->heading() ?></h1>
 <?php endif ?>
-</section>
