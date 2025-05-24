d982cc70e072f72f6d67d4b1f794bc7841df8c61
aengustukel <84077392+aengustukel@users.noreply.github.com>
2025-05-24 10:35:22 +0100

    navigation snippet

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -20,18 +20,5 @@
 
 </head>
 <body class="page-<?= $template ?><?= $template != "home" ? " not-home" : "" ?>">
-  <header class="header">
-    <?php if ($logo = $site->logo()->toFile()): ?>
-    <a class="" href="<?= $site->url() ?>">
-      <img class="" src="<?= $logo->url() ?>" alt="<?= $logo->alt() ?>" width="106" height="32"/>
-    </a>
-    <?php endif ?>
-
-    <nav class="menu">
-      <?php foreach ($site->children()->listed() as $item): ?>
-      <a <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><?= $item->title()->esc() ?></a>
-      <?php endforeach ?>
-    </nav>
-  </header>
-
-  <main class="main">
+<?php snippet('nav'); ?>
+<main class="main">
