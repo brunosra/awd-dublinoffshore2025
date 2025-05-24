8802393d5823022d500a0905eb0979eca73dd790
Bruno Almeida <brunosra@gmail.com>
2025-05-24 17:38:50 +0100

    add burger animation

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,5 +1,6 @@
 <?php 
   $assetManager->add('css', vite()->asset('assets/scss/snippets/nav.scss'));
+  $assetManager->add('js', vite()->asset('assets/js/snippets/nav.js'));
 ?>
 
 <header class="header">
@@ -9,6 +10,11 @@
       <?php foreach ($site->children()->listed() as $item): ?>
         <li><a <?php e($item->isOpen(), 'aria-current="page"') ?> href="<?= $item->url() ?>"><?= $item->title()->esc() ?></a></li>
       <?php endforeach ?>
-      </ul>
+    </ul>
+    <a href="#" class="burger">
+      <div id="burger1"></div>
+      <div id="burger2"></div>
+      <div id="burger3"></div>
+    </a>
   </nav>
 </header>
