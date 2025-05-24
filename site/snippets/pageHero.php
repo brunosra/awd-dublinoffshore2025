8a464e5a731691eddcf861ffcf6bae3128297983
aengustukel <84077392+aengustukel@users.noreply.github.com>
2025-05-24 19:51:57 +0100

    Page Hero strcuture

--- site/snippets/pageHero.php
+++ site/snippets/pageHero.php
@@ -1,20 +1,27 @@
-<?php $isUpdate = $page->parent('updates'); ?>
+<?php
+$template = $page->intendedTemplate()->name();
+?>
 
-<?php if ($isUpdate): ?>
+<section>
+
+<?php if ($template === 'updates'): ?>
+  <h1><?= $page->title()->html() ?></h1>
+
+<?php elseif ($template === 'post'): ?>
   <h5><?= $page->parent()->title()->esc() ?></h5>
   <h1><?= $page->title()->html() ?></h1>
 
   <?php if ($page->cover()->isNotEmpty()): ?>
-      <?php foreach ($page->cover()->toFiles() as $image): ?>
-    <?php
-    $sizes = "(min-width: 1200px) 50vw,
-              (min-width: 900px) 33vw,
-              (min-width: 600px) 50vw,
-              100vw";
-    ?>
-    <picture>
-      <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-      <img
+    <?php foreach ($page->cover()->toFiles() as $image): ?>
+      <?php
+      $sizes = "(min-width: 1200px) 50vw,
+                (min-width: 900px) 33vw,
+                (min-width: 600px) 50vw,
+                100vw";
+      ?>
+      <picture>
+        <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+        <img
           class=""
           alt="<?= $image->alt() ?>"
           data-src="<?= $image->resize(300)->url() ?>"
@@ -22,14 +29,14 @@
           sizes="<?= $sizes ?>"
           width="<?= $image->resize(310)->width() ?>"
           height="<?//= $image->resize(500)->height() ?>">
-    </picture>
+      </picture>
     <?php endforeach ?>
   <?php else: ?>
     <!-- no placeholder image -->
   <?php endif ?>
 
-<?php elseif (isset($block)): ?>
-  <?php if ($block->background()->isNotEmpty()): ?>
+<?php else: ?>
+  <?php if (isset($block) && $block->background()->isNotEmpty()): ?>
     <?php foreach ($block->background()->toFiles() as $image): ?>
       <?php
       $sizes = "(min-width: 1200px) 50vw,
@@ -52,5 +59,8 @@
   <?php else: ?>
     <!-- no background image -->
   <?php endif ?>
+
   <h1><?= $block->heading() ?></h1>
 <?php endif ?>
+
+</section>
