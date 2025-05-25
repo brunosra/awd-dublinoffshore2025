c489fc06285f283fdd5375400c37d1fb3f267408
Bruno Almeida <brunosra@gmail.com>
2025-05-25 12:25:12 +0100

    add hot

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,4 +1,5 @@
 <?php snippet('header') ?>
+<?php snippet('pageHero') ?>
   <h1>Heading 1</h1>
   <h2>Heading 2</h2>
   <h3>Heading 3</h3>
@@ -8,4 +9,12 @@
   <p>Paragraph</p>
   <p class="small">small</p>
   <p class="nav">nav</p>
+  <hr>
+  <ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3 - <a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipiscing elit. Nam a placerat orci. Vivamus dictum mi vel sapien accumsan, in finibus lacus faucibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla eget pellentesque justo, ac tempus erat. Maecenas placerat lorem ut risus ullamcorper consectetur. Aenean venenatis sapien in nunc maximus venenatis. Quisque vel porta elit, vitae viverra purus.</li>
+    <li>Item 4</li>
+    <li>Item 5</li>
+  </ul>
 <?php snippet('footer') ?>
