fdc18f52bd60cf6571aeb8bc073b34d6ad9386c4
Bruno Almeida <brunosra@gmail.com>
2025-05-26 19:03:40 +0100

    work in progress: pageHero

--- site/snippets/pageHero.php
+++ site/snippets/pageHero.php
@@ -22,66 +22,67 @@ switch($pageTemplate) {
 ?>
 
 <section class="page-hero<?= " ".$finalClass ?>">
-<div class="container">
 <?php if ($template === 'updates'): ?>
+  <div class="container">
   <h1><?= $page->title()->html() ?></h1>
-
-<?php elseif ($template === 'post'): ?>
-  <h5><?= $page->parent()->title()->esc() ?></h5>
-  <h1><?= $page->title()->html() ?></h1>
-
-  <?php if ($page->cover()->isNotEmpty()): ?>
-    <?php foreach ($page->cover()->toFiles() as $image): ?>
-      <?php
-      $sizes = "(min-width: 1200px) 50vw,
-                (min-width: 900px) 33vw,
-                (min-width: 600px) 50vw,
-                100vw";
-      ?>
-      <picture>
-        <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-        <img
-          class=""
-          alt="<?= $image->alt() ?>"
-          data-src="<?= $image->resize(300)->url() ?>"
-          data-srcset="<?= $image->srcset() ?>"
-          sizes="<?= $sizes ?>"
-          width="<?= $image->resize(310)->width() ?>"
-          height="<?//= $image->resize(500)->height() ?>">
-      </picture>
-    <?php endforeach ?>
-  <?php else: ?>
-    <!-- no placeholder image -->
-  <?php endif ?>
-
-<?php else: ?>
-  <?php if (isset($block) && $block->background()->isNotEmpty()): ?>
-    <?php foreach ($block->background()->toFiles() as $image): ?>
-      <?php
-      $sizes = "(min-width: 1200px) 50vw,
-                (min-width: 900px) 33vw,
-                (min-width: 600px) 50vw,
-                100vw";
-      ?>
-      <picture>
-        <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-        <img
-          class=""
-          alt="<?= $image->alt() ?>"
-          data-src="<?= $image->resize(300)->url() ?>"
-          data-srcset="<?= $image->srcset() ?>"
-          sizes="<?= $sizes ?>"
-          width="<?//= $image->resize(310)->width() ?>"
-          height="<?//= $image->resize(500)->height() ?>">
-      </picture>
-    <?php endforeach ?>
-  <?php else: ?>
-    <!-- no background image -->
-  <?php endif ?>
-
-  <h1><?= isset($block) ? $block->heading() : "" ?></h1>
-<?php endif ?>
 </div>
 
+<?php elseif ($template === 'post'): ?>
+  <div class="container">
+    <h5><?= $page->parent()->title()->esc() ?></h5>
+    <h1><?= $page->title()->html() ?></h1>
 
+    <?php if ($page->cover()->isNotEmpty()): ?>
+      <?php foreach ($page->cover()->toFiles() as $image): ?>
+        <?php
+        $sizes = "(min-width: 1200px) 50vw,
+                  (min-width: 900px) 33vw,
+                  (min-width: 600px) 50vw,
+                  100vw";
+        ?>
+        <picture>
+          <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+          <img
+            class=""
+            alt="<?= $image->alt() ?>"
+            data-src="<?= $image->resize(300)->url() ?>"
+            data-srcset="<?= $image->srcset() ?>"
+            sizes="<?= $sizes ?>"
+            width="<?= $image->resize(310)->width() ?>"
+            height="<?//= $image->resize(500)->height() ?>">
+        </picture>
+      <?php endforeach ?>
+    <?php else: ?>
+      <!-- no placeholder image -->
+    <?php endif ?>
+  </div>
+
+<?php else: ?>
+    <?php if (isset($block) && $block->background()->isNotEmpty()): ?>
+      <?php foreach ($block->background()->toFiles() as $image): ?>
+        <?php
+        $sizes = "(min-width: 1200px) 50vw,
+                  (min-width: 900px) 33vw,
+                  (min-width: 600px) 50vw,
+                  100vw";
+        ?>
+        <picture>
+          <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+          <img
+            class=""
+            alt="<?= $image->alt() ?>"
+            data-src="<?= $image->resize(300)->url() ?>"
+            data-srcset="<?= $image->srcset() ?>"
+            sizes="<?= $sizes ?>"
+            width="<?//= $image->resize(310)->width() ?>"
+            height="<?//= $image->resize(500)->height() ?>">
+        </picture>
+      <?php endforeach ?>
+    <?php else: ?>
+      <!-- no background image
+    <?php endif ?> -->
+  <div class="container">
+    <h1><?= isset($block) ? $block->heading() : "" ?></h1>
+  </div>
+<?php endif ?>
 </section>
