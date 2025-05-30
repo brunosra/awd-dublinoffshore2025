baf2d75f4ba43cbeaf5d43f494c346023bca5f6e
aengustukel <84077392+aengustukel@users.noreply.github.com>
2025-05-30 21:15:26 +0100

    page grid php

--- site/snippets/blocks/page_grid.php
+++ site/snippets/blocks/page_grid.php
@@ -1,33 +1,37 @@
 <?php 
-  $assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/team.scss'));
+  $assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/page_grid.scss'));
 ?>
 
-<section class="team">
-  <h2><?= $block->heading()->esc() ?></h2>
-  <?php $members = $block->teamMembers()->toStructure(); foreach ($members as $member): ?>
-    <article class="">
-      <?php foreach ($member->image()->toFiles() as $image): ?>
-        <?php
-        $sizes = "(min-width: 1200px) 50vw,
-                  (min-width: 900px) 33vw,
-                  (min-width: 600px) 50vw,
-                  100vw";
-        ?>
-        <picture>
-          <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-          <img
-              class=""
-              alt="<?= $image->alt() ?>"
-              data-src="<?= $image->resize(300)->url() ?>"
-              data-srcset="<?= $image->srcset() ?>"
-              sizes="<?= $sizes ?>"
-              width="<?= $image->resize(346)->width() ?>"
-              height="<?//= $image->resize(500)->height() ?>">
-        </picture>
+<section class="">
+  <?= $block->contentArea() ?>
+
+  <?php $linkedPages = $block->linkedPages()->toStructure(); foreach ($linkedPages as $linkedPage): ?>
+      <?php foreach ($linkedPage->page()->toPages() as $servicePage): ?>
+        <a href="<?= $servicePage->url() ?>">
+          <h4><?= $servicePage->title() ?></h4>
+          <?= $servicePage->details() ?>
+      </a>
       <?php endforeach ?>
-      <h6><?= $member->memberName() ?></h6>
-      <p class="small"><?= $member->position() ?></p>
-      <p class="small"><?= $member->bio() ?></p>
-    </article> 
   <?php endforeach ?>
+
+  <?php foreach ($block->background()->toFiles() as $image): ?>
+    <?php
+    $sizes = "(min-width: 1200px) 50vw,
+              (min-width: 900px) 33vw,
+              (min-width: 600px) 50vw,
+              100vw";
+    ?>
+    <picture>
+      <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+      <img
+          class=""
+          alt="<?= $image->alt() ?>"
+          data-src="<?= $image->resize(300)->url() ?>"
+          data-srcset="<?= $image->srcset() ?>"
+          sizes="<?= $sizes ?>"
+          width="<?= $image->resize(346)->width() ?>"
+          height="<?//= $image->resize(500)->height() ?>">
+    </picture>
+  <?php endforeach ?>
+
 </section>
