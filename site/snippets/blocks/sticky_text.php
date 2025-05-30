12f06a3f28c9b55ca8ef7e8836cb084a12587fc4
Hasibullah <hasibullah.mahmood@ruul.io>
2025-05-30 20:08:32 +0300

    Implement sticky text block styles and structure in SCSS and PHP.

--- site/snippets/blocks/sticky_text.php
+++ site/snippets/blocks/sticky_text.php
@@ -1,29 +1,32 @@
-<?php 
-  $assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/sticky-text.scss'));
+<?php
+$assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/sticky-text.scss'));
 ?>
 
 <section class="sticky-text">
-  <div>
-  <?= $block->textArea() ?>
+  <div class="sticky-text__container container">
+    <div class="sticky-text__content">
+      <h2><?= strip_tags($block->textArea()) ?></h2>
+    </div>
+    <div class="sticky-text__articles">
+      <?php $contentBlocks = $block->contentBlock()->toStructure();
+      foreach ($contentBlocks as $contentBlock): ?>
+        <article class="sticky-text__article">
+          <?php if ($contentBlock->icon()->isNotEmpty()): ?>
+            <span class="sticky-text__article-icon">
+              <?= svg('/assets/icons/' . $contentBlock->icon()) ?>
+            </span>
+          <?php endif ?>
+
+          <div class="sticky-text__article-content">
+            <?php if ($contentBlock->heading()->isNotEmpty()): ?>
+              <h3>
+                <?= strip_tags($contentBlock->heading()) ?>
+              </h3>
+            <?php endif ?>
+            <?= $contentBlock->contentArea() ?>
+          </div>
+        </article>
+      <?php endforeach ?>
+    </div>
   </div>
-
-  <?php $contentBlocks = $block->contentBlock()->toStructure(); foreach ($contentBlocks as $contentBlock): ?>
-  <article class="">
-    <?php if ($contentBlock->icon()->isNotEmpty()): ?>
-    <div>
-      <?= svg('/assets/icons/' . $page->icon()) ?>
-    </div>
-    <?php endif ?>
-    <div>
-    <?php if ($contentBlock->heading()->isNotEmpty()): ?>
-    <div>
-      <?= $contentBlock->heading() ?>
-    </div>
-    <?php endif ?>
-      <?= $contentBlock->contentArea() ?>
-    </div>
-  </article> 
-  <?php endforeach ?>
-
-
 </section>
