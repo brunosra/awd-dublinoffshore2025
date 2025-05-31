e95b3bfd211e0c36f4047f6c4cbd9116f7f02ff3
Bruno Almeida <brunosra@gmail.com>
2025-05-31 19:21:44 +0100

    improve input styles

--- site/snippets/blocks/lrd.php
+++ site/snippets/blocks/lrd.php
@@ -52,72 +52,72 @@
           <div class="message"></div>
         </div>
       </form>
-    </div>
-  </div>
-  
-
-  <div>
-    <dialog id="loading">
-      <h1>Loading...</h1>
-    </dialog>
-    <!-- Create input sliders -->
-    <div id="sliders">
-      <div class="slider-group">
-        <div class="slider-title">
-          <p>Diameter <span>(m)</span></p>
-          <input type="text" class="slider-input" disabled id="input-LRD-dia" />
+      <!-- Start APP -->
+      <div class="lrd-app">
+        <dialog id="loading">
+          <h1>Loading...</h1>
+        </dialog>
+        <!-- Create input sliders -->
+        <div id="sliders">
+          <div class="slider-group">
+            <div class="slider-title">
+              <p>Diameter <span>(m)</span></p>
+              <input type="text" class="slider-input" disabled id="input-LRD-dia" />
+            </div>
+            <div class="main-slider" data-max="5" data-min="2.5">
+              <input id="LRD-dia" type="range" min="2.5" max="5" step="0.05" value="3.75" class="slider">
+            </div>
+          </div>
+          <div class="slider-group">
+            <div class="slider-title">
+              <p>Length <span>(m)</span></p>
+              <input type="text" class="slider-input" disabled id="input-LRD-hei" />
+            </div>
+            <div class="main-slider" data-min="10" data-max="22.5">
+              <input id="LRD-hei" type="range" min="10" max="22.5" step="0.25" value="17.5" class="slider">
+            </div>
+          </div>
+          <div class="slider-group">
+            <div class="slider-title">
+              <p>Axes horizontal offset <span>(m)</span></p>
+              <input type="text" class="slider-input" disabled id="input-axes-horz-dist" />
+            </div>
+            <div class="main-slider" data-max="2" data-min="0">
+              <input id="axes-horz-dist" type="range" min="0" max="2" step="0.1" value="1" class="slider">
+            </div>
+          </div>
+          <div class="slider-group">
+            <div class="slider-title">
+              <p>Axes vertical offset <span>(m)</span></p>
+              <input type="text" class="slider-input" disabled id="input-axes-vert-dist" />
+            </div>
+            <div class="main-slider" data-max="10" data-min="5">
+              <input id="axes-vert-dist" type="range" min="5" max="10" step="0.1" value="5" class="slider">
+            </div>
+          </div>
+          <div class="slider-group">
+            <div class="slider-title">
+              <p>Mooring declination from vertical <span>(deg)</span></p>
+              <input type="text" class="slider-input" disabled id="input-mooring-decl" />
+            </div>
+            <div class="main-slider" data-max="85" data-min="30">
+              <input id="mooring-decl" type="range" min="30" max="85" step="1" value="70" class="slider">
+            </div>
+          </div>
         </div>
-        <div class="main-slider" data-max="5" data-min="2.5">
-          <input id="LRD-dia" type="range" min="2.5" max="5" step="0.05" value="3.75" class="slider">
+        <!-- Create graph div -->
+        <div class="graph-area">
+          <div id="img-placeholder">
+            <?php snippet('lrd-image'); ?>
+          </div>
+          <div id="graph"></div>
+        </div>
+        <div class="buttons" style="display: flex; gap: 20px; justify-content: center;">
+          <button class="primary" id="download-pdf">Download Specification</button>
+          <button class="primary" id="download-csv">Download Stiffness Curve</button>
         </div>
       </div>
-      <div class="slider-group">
-        <div class="slider-title">
-          <p>Length <span>(m)</span></p>
-          <input type="text" class="slider-input" disabled id="input-LRD-hei" />
-        </div>
-        <div class="main-slider" data-min="10" data-max="22.5">
-          <input id="LRD-hei" type="range" min="10" max="22.5" step="0.25" value="17.5" class="slider">
-        </div>
-      </div>
-      <div class="slider-group">
-        <div class="slider-title">
-          <p>Axes horizontal offset <span>(m)</span></p>
-          <input type="text" class="slider-input" disabled id="input-axes-horz-dist" />
-        </div>
-        <div class="main-slider" data-max="2" data-min="0">
-          <input id="axes-horz-dist" type="range" min="0" max="2" step="0.1" value="1" class="slider">
-        </div>
-      </div>
-      <div class="slider-group">
-        <div class="slider-title">
-          <p>Axes vertical offset <span>(m)</span></p>
-          <input type="text" class="slider-input" disabled id="input-axes-vert-dist" />
-        </div>
-        <div class="main-slider" data-max="10" data-min="5">
-          <input id="axes-vert-dist" type="range" min="5" max="10" step="0.1" value="5" class="slider">
-        </div>
-      </div>
-      <div class="slider-group">
-        <div class="slider-title">
-          <p>Mooring declination from vertical <span>(deg)</span></p>
-          <input type="text" class="slider-input" disabled id="input-mooring-decl" />
-        </div>
-        <div class="main-slider" data-max="85" data-min="30">
-          <input id="mooring-decl" type="range" min="30" max="85" step="1" value="70" class="slider">
-        </div>
-      </div>
-    </div>
-    <!-- Create graph div -->
-    <div class="graph-area">
-      <div id="img-placeholder">
-        <?php snippet('lrd-image'); ?>
-      </div>
-      <div id="graph"></div>
-    </div>
-    <div class="buttons" style="display: flex; gap: 20px; justify-content: center;">
-      <button class="primary" id="download-pdf">Download Specification</button>
-      <button class="primary" id="download-csv">Download Stiffness Curve</button>
+      <!-- END APP -->
     </div>
   </div>
 </section>
