aca96059eb74679329a3050a181a132f199a0479
aengustukel <84077392+aengustukel@users.noreply.github.com>
2025-06-01 20:14:38 +0100

    footer fix, privacy policy

--- site/templates/legal.php
+++ site/templates/legal.php
@@ -1,20 +1,9 @@
 <?php snippet('header') ?>
+<?php snippet('page_hero') ?>
 
-<section class="page_hero text-center light-blue-theme">
-  <div class="grid-center container">
-      <div class="col-6" data-aos="fade-up" data-aos-duration="800">
-        <h3><?= $page->headline()->html() ?></h3>
-      </div>
-  </div>
-</section>  
-
-<section class="tight-padding">
-
-<div class="grid-center container">
-      <div class="col-7" data-aos="fade-up" data-aos-duration="800" data-delay="300">
-      <?= $page->legaltext()->kt() ?>
-      </div>
-  </div>
+<section>
+  <?= $page->legaltext()->kt() ?>
+</section>
 
 <?php snippet('footer') ?>
 
