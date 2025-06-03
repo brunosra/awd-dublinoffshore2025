c3b468578980429fcc36a4eb121e654b9e0e0cc9
Hasibullah <hasibullah.mahmood@ruul.io>
2025-06-03 09:07:20 +0300

    Implement offices block with responsive styles and structured layout, enhancing visual presentation and accessibility of office information.

--- site/snippets/blocks/offices.php
+++ site/snippets/blocks/offices.php
@@ -1,36 +1,43 @@
-<?php 
-  $assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/offices.scss'));
+<?php
+$assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/offices.scss'));
 ?>
 
 <section class="offices">
-  <h2><?= $block->heading()->esc() ?></h2>
-  <?php
-  $addresses = $site->addresses()->toStructure();
-  foreach ($addresses as $address): ?>
-  <a class="" href="<?= $address->mapLocation()->url() ?>" target="_blank" rel="noopener nofollow">
-    <div>
-      <?php foreach ($address->image()->toFiles() as $image): ?>
+  <div class="container offices__container">
+    <h2 class="offices__heading">Our offices</h2>
+    <ul class="offices__list">
       <?php
-      $sizes = "(min-width: 1200px) 50vw,
+      $addresses = $site->addresses()->toStructure();
+      foreach ($addresses as $address): ?>
+        <li class="offices__item">
+          <a class="" href="<?= $address->mapLocation()->url() ?>" target="_blank" rel="noopener nofollow">
+            <?php foreach ($address->image()->toFiles() as $image): ?>
+              <?php
+              $sizes = "(min-width: 1200px) 50vw,
                 (min-width: 900px) 33vw,
                 (min-width: 600px) 50vw,
                 100vw";
-      ?>
-      <picture>
-        <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-        <img
-            class=""
-            alt="<?= $image->alt() ?>"
-            data-src="<?= $image->resize(300)->url() ?>"
-            data-srcset="<?= $image->srcset() ?>"
-            sizes="<?= $sizes ?>"
-            width="<?= $image->resize(456)->width() ?>"
-            height="<?//= $image->resize(500)->height() ?>">
-      </picture>
+              ?>
+              <picture>
+                <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
+                <img
+                  class=""
+                  alt="<?= $image->alt() ?>"
+                  data-src="<?= $image->resize(300)->url() ?>"
+                  data-srcset="<?= $image->srcset() ?>"
+                  sizes="<?= $sizes ?>"
+                  width="<?= $image->resize(456)->width() ?>"
+                  height="<? //= $image->resize(500)->height() 
+                          ?>">
+              </picture>
+            <?php endforeach ?>
+            <div class="offices__item-content">
+              <h6 class="small"><?= $address->city()->esc() ?>,&nbsp;<?= $address->country()->esc() ?></h6>
+              <h6 class="small"><?= $address->postcode()->esc() ?></h6>
+            </div>
+          </a>
+        </li>
       <?php endforeach ?>
-      <p class="small"><?= $address->city()->esc() ?>,&nbsp;<?= $address->country()->esc() ?></p>
-      <p class="small"><?= $address->postcode()->esc() ?></p>
-    </div>
-  </a>
-  <?php endforeach ?>
+    </ul>
+  </div>
 </section>
--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,35 +1,4 @@
 <?php snippet('header') ?>
 <?php snippet('contact_block') ?>
-
-<section>
-  <?php $addresses = $site->addresses()->toStructure();
-  foreach ($addresses as $address): ?>
-
-    <a href="<?= $address->mapLocation()->url() ?>">
-      <?php foreach ($address->image()->toFiles() as $image): ?>
-        <?php
-        $sizes = "(min-width: 1200px) 50vw,
-                  (min-width: 900px) 33vw,
-                  (min-width: 600px) 50vw,
-                  100vw";
-        ?>
-        <picture>
-          <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-          <img
-            class=""
-            alt="<?= $image->alt() ?>"
-            data-src="<?= $image->resize(300)->url() ?>"
-            data-srcset="<?= $image->srcset() ?>"
-            sizes="<?= $sizes ?>"
-            width="<?= $image->resize(456)->width() ?>"
-            height="<? //= $image->resize(500)->height() 
-                    ?>">
-        </picture>
-      <?php endforeach ?>
-      <h5><?= $address->city() ?>, <?= $address->country() ?></h5>
-      <h5><?= $address->postcode() ?></h5>
-    </a>
-  <?php endforeach ?>
-</section>
-
+<?php snippet('blocks/offices') ?>
 <?php snippet('footer') ?>
