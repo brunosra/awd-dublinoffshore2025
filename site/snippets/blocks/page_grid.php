2e8ba21a1ad869e3aa85a66fc88afa89f26b54a7
Hasibullah <hasibullah.mahmood@ruul.io>
2025-06-03 22:38:59 +0300

    Add page grid block styles and structure, implementing responsive design and background image support for enhanced layout and visual appeal.

--- site/snippets/blocks/page_grid.php
+++ site/snippets/blocks/page_grid.php
@@ -1,37 +1,35 @@
-<?php 
-  $assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/page-grid.scss'));
+<?php
+$assetManager->add('css', vite()->asset('assets/scss/snippets/blocks/page-grid.scss'));
+
+$image = $block->background()->toFiles()->first();
+$backgroundUrl = $image ? $image->url() : '';
 ?>
 
-<section class="">
-  <?= $block->contentArea() ?>
-
-  <?php $linkedPages = $block->linkedPages()->toStructure(); foreach ($linkedPages as $linkedPage): ?>
-      <?php foreach ($linkedPage->page()->toPages() as $servicePage): ?>
-        <a href="<?= $servicePage->url() ?>">
-          <h4><?= $servicePage->title() ?></h4>
-          <?= $servicePage->details() ?>
-      </a>
+<section class="page-grid" style="background-image: url('<?= $backgroundUrl ?>');">
+  <div class="container page-grid__container">
+    <header class="page-grid__header">
+      <?= $block->contentArea() ?>
+    </header>
+    <div class="page-grid__list">
+      <?php $linkedPages = $block->linkedPages()->toStructure();
+      foreach ($linkedPages as $linkedPage): ?>
+        <?php foreach ($linkedPage->page()->toPages() as $servicePage): ?>
+          <a href="<?= $servicePage->url() ?>" class="page-grid__card">
+            <h4><?= $servicePage->title() ?></h4>
+            <div class="page-grid__card-content">
+              <div class="page-grid__card-paragraphs">
+                <?= $servicePage->details() ?>
+                <!-- TEMP: should be removed later -->
+                <p>Client representation</p>
+                <p>Technical oversight and support</p>
+                <p>Quality assurance and control</p>
+                <p>Health, Safety, and Environmental (HSE) management​</p>
+              </div>
+              <span class="page-grid__card-arrow"><img src="<?= vite()->asset('assets/images/arrow.svg') ?>" alt="Read More" width="10" height="10"></span>
+            </div>
+          </a>
+        <?php endforeach ?>
       <?php endforeach ?>
-  <?php endforeach ?>
-
-  <?php foreach ($block->background()->toFiles() as $image): ?>
-    <?php
-    $sizes = "(min-width: 1200px) 50vw,
-              (min-width: 900px) 33vw,
-              (min-width: 600px) 50vw,
-              100vw";
-    ?>
-    <picture>
-      <source srcset="<?= $image->srcset('webp') ?>" sizes="<?= $sizes ?>" type="image/webp">
-      <img
-          class=""
-          alt="<?= $image->alt() ?>"
-          data-src="<?= $image->resize(300)->url() ?>"
-          data-srcset="<?= $image->srcset() ?>"
-          sizes="<?= $sizes ?>"
-          width="<?= $image->resize(346)->width() ?>"
-          height="<?//= $image->resize(500)->height() ?>">
-    </picture>
-  <?php endforeach ?>
-
+    </div>
+  </div>
 </section>
