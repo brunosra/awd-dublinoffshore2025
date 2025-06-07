541e188af5514af14d33f7a9bb680d9226712f1b
Bruno Almeida <brunosra@gmail.com>
2025-06-07 12:44:00 +0100

    fixedlayouts paddings

--- site/snippets/blocks/layouts.php
+++ site/snippets/blocks/layouts.php
@@ -5,7 +5,7 @@
 <!-- https://getkirby.com/docs/reference/panel/fields/layout -->
 
 <?php foreach ($block->layout()->toLayouts() as $layout): ?>
-<section class="layouts<?= $layout->paddingTop() ? '' : ' no-padding-top' ?><?= $layout->paddingBottom() ? '' : ' no-padding-bottom' ?>" id="<?= $layout->id() ?>">
+<section class="layouts<?= $layout->paddingTop()->toBool() ? '' : ' no-padding-top' ?><?= $layout->paddingBottom()->toBool() ? '' : ' no-padding-bottom' ?>" id="<?= $layout->id() ?>">
   <div class="container">
     <?php foreach ($layout->columns() as $column): ?>
     <div class="blocks">
