a2d52d0b7d1d1bee25d056db0a76970d068cbe50
Bruno Almeida <brunosra@gmail.com>
2025-06-07 18:30:49 +0100

    add icons list

--- site/snippets/blocks/icon_list.php
+++ site/snippets/blocks/icon_list.php
@@ -4,9 +4,11 @@
 
 <div class="icon-list">
 <?php $icons = $block->iconList()->toStructure(); foreach ($icons as $icon): ?>
-  <?php foreach ($icon->icon()->toFiles() as $image): ?>
-  <img src="<?= $image->url() ?>">
-  <?php endforeach ?>
-  <h6><?= $icon->description() ?></h6>
+  <div class="icon-list-item">
+    <?php foreach ($icon->icon()->toFiles() as $image): ?>
+    <img src="<?= $image->url() ?>">
+    <?php endforeach ?>
+    <p class="h6"><?= $icon->description() ?></p>
+  </div>
 <?php endforeach ?>
 </div>
